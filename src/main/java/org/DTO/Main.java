b571d23f5cc45e1163a0c1b096710c7c8f361131
Amber0444 <alexander.shevelev04@gmail.com>
2025-12-07 18:17:35 +0500

    Initial commit

---
+++ src/main/java/org/DTO/Main.java
@@ -0,0 +1,7 @@
+package org.DTO;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/org/DTO/NotificationDTO.java
@@ -0,0 +1,43 @@
+package org.DTO;
+
+import lombok.Data;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+public class NotificationDTO {
+    private UUID uuid;
+    private NotificationType type;
+    private Sender sender;
+    private Receiver receiver;
+    private Notice notice;
+
+    @Data
+    public static class Sender {
+        private String name;
+        private String contact;
+    }
+
+    @Data
+    public static class Receiver {
+        private String contact;
+    }
+
+    @Data
+    public static class Notice {
+        private String topic;
+        private String body;
+        private List<Content> content;
+        private String sentAt;
+    }
+
+    @Data
+    public static class Content {
+        private String fileName;
+        private String data;
+    }
+
+    public enum NotificationType {
+        SMS, EMAIL, PUSH
+    }
+}
